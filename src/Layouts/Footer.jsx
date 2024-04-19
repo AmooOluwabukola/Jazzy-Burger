ca560ba52c0eaf7f64943371fb64f9522fc8e04b
Amoo Oluwabukola <amoooluwabukola@outlook.com>
2024-04-19 19:13:22 +0100

    modified Footer.jsx, Footer.css

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <>
       <footer className='bg-black py-2'> 
-        <section className='container my-4'>
+        <section className='container my-4 footer-section'>
           <div className='d-lg-flex justify-content-between  gap-4'>
            
             {/* parent div */}
@@ -22,18 +22,17 @@ const Footer = () => {
                  {/* Images for 24/7 support */}
             <div className='d-lg-flex text-white'>
               <img src={Group27} alt="" />
-              <h4 className=' mx-2 fs-3'>24/7 <span>Support</span></h4>
+              <h4 className=' mx-2 fs-3'>24/7 <span className='support'>Support</span></h4>
               
             </div>
             <div className='d-lg-flex text-white'>
               <img src={Vector} alt="" />
-              <h4 className=' mx-2 fs-3'>100%   <span>Payment Secured</span></h4>
-             
+              <h4 className=' mx-2 fs-2'>100%   <span className='payment'>Payment Secured</span></h4>
             </div>
             </div>
             
             {/* Add your two new images for links */}
-            <div className='d-lg-flex gap-4'>
+            <div className='d-lg-flex gap-4 w-50'>
               <img src={otherImage1} alt="Appstore" className='stores' />
               <img src={otherImage2} alt="Playstore" className='stores' />
             </div>
